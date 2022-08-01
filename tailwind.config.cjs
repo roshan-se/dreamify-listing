96e32f6ff3d7cd7fd91cef3e1667e5059ab8cd1b
er-roshan <roshan.adhikari.se@gmail.com>
2022-08-01 23:14:36 +0545

    Color and Fonts preset variables added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: 'var(--color-primary)',
+				secondary: 'var(--color-secondary)',
+				success: 'var(--color-success)',
+				warning: 'var(--color-warning)',
+				danger: 'var(--color-danger)',
+				neutral: 'var(--color-neutral)',
+			},
+		},
 	},
 	plugins: [],
 };
