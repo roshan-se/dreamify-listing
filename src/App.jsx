a3a3d1f75ebd88922ecf38c69a0db0b275a7a98c
er-roshan <roshan.adhikari.se@gmail.com>
2022-08-06 01:12:49 +0545

    Feat: Navbar and Footer added globally;

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
 	return (
 		<>
 			<Router>
+				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />}></Route>
 				</Routes>
+				<Footer />
 			</Router>
 		</>
 	);
