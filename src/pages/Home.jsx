5fce29ad45d71eba9401b82eee7fd02a256b6f5a
er-roshan <roshan.adhikari.se@gmail.com>
2022-08-06 01:13:57 +0545

    Feat: New components added to Home page;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import EventList from '../components/EventList';
 import FeaturedCategories from '../components/FeaturedCategories';
 import HeroBanner from '../components/HeroBanner';
+import PopularListing from '../components/PopularListing';
+import PopularPlaces from '../components/PopularPlaces';
 
 const Home = () => {
 	return (
 		<div>
 			<HeroBanner />
 			<FeaturedCategories />
+			<PopularListing />
+			<PopularPlaces />
+			<EventList />
 		</div>
 	);
 };
