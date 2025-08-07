14d1a8c4295f4517607c2dae4d4d50969f79c5a9
roshan-se <roshan.adhikari.se@gmail.com>
2025-08-07 08:05:22 +0800

    Added AOS animation package

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import { useEffect } from 'react';
 
 function App() {
+	useEffect(() => {
+		AOS.init({
+			duration: 800,
+			easing: 'ease-in-out',
+			once: false,
+			mirror: true,
+		});
+
+		AOS.refresh();
+
+	}, []);
 	return (
 		<>
 			<Router>
